c7c8edeac50ef4dfccadcd36751281a94d64d456
lwesy <lw@esyoil.com>
2018-05-24 10:44:34 +0200

    Add todoapp

--- src/pages/TodoApp.js
+++ src/pages/TodoApp.js
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { Component } from 'react'
+import styled from 'styled-components'
 
-export default () => (
-  <div>
-    <h1>Todos</h1>
-  </div>
-)
+const Todo = styled.li`
+  text-decoration: ${props => (props.completed ? 'line-through' : '')};
+  :hover {
+    background: black;
+  }
+`
+
+export default class extends Component {
+  state = {
+    input: '',
+    todos: [
+      { id: 1, body: 'Todo 1', completed: false },
+      { id: 2, body: 'Todo 2', completed: false },
+      { id: 3, body: 'Todo 3', completed: true }
+    ]
+  }
+
+  onInputChange = event => {
+    this.setState({ input: event.target.value })
+  }
+
+  onClickHandler = () => {
+    this.setState(prevState => ({
+      todos: [
+        ...prevState.todos,
+        {
+          id: prevState.todos.length + 1,
+          body: prevState.input,
+          completed: false
+        }
+      ]
+    }))
+  }
+
+  onTodoToggleHandler = id => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(
+        todo =>
+          todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    }))
+  }
+
+  renderTodos = () =>
+    this.state.todos.map(todo => (
+      <Todo
+        onClick={() => this.onTodoToggleHandler(todo.id)}
+        key={todo.id}
+        completed={todo.completed}
+      >
+        {todo.body}
+      </Todo>
+    ))
+
+  render() {
+    return (
+      <div>
+        <h1>Todos</h1>
+        <input value={this.state.input} onChange={this.onInputChange} />
+        <button onClick={this.onClickHandler}>add</button>
+        <br />
+        {this.renderTodos()}
+      </div>
+    )
+  }
+}
