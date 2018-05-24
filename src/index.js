289a34376ac5122c4807b9416ea68accf2d67f81
lwesy <lw@esyoil.com>
2018-05-24 08:50:54 +0200

    Install typograhy.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import Typography from 'typography'
 
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.666
+})
+
+typography.injectStyles()
 
 const app = (
   <BrowserRouter>
