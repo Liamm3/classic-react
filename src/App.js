3a204d90d10a9c9d354c1c15ceaaac8efb1f3f19
lwesy <lw@esyoil.com>
2018-05-24 08:30:37 +0200

    Add some dummy pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
+import Home from './pages/Home'
+import News from './pages/News'
+import TodoApp from './pages/TodoApp'
+import Products from './pages/Products'
+
 export default () => (
   <Switch>
-    <Route path="/" render={() => <h1>Home</h1>} />
+    <Route path="/todos" component={TodoApp} />
+    <Route path="/products" component={Products} />
+    <Route path="/news" component={News} />
+    <Route path="/" component={Home} />
   </Switch>
 )
---
+++ src/pages/News.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    <h1>News</h1>
+  </div>
+)
